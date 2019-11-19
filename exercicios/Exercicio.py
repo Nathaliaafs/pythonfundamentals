01d0f9af5ff0f272c4a71142f8509c3138ddfcb0
Nathalia Silva <nath.afs9@gmail.com>
2019-11-18 23:19:23 -0200

    aula 05

---
+++ exercicios/Exercicio.py
@@ -0,0 +1,77 @@
+# # 1.Criando um programa que imprima o nome do seu time
+
+# #print(input('Qual é o seu time:'))
+
+# # 2. Faça um progrma que peça um  número e imprima esse número 
+
+# # N1 = input('Digite um número:')
+# # print(N1)
+
+# # 3. Faça um programa que receba 'F' ou 'M' e mostre Feminino ou Masculino.
+
+# # sexo = str(input('Qual é o seu sexo:'))
+
+# # sexo = sexo.capitalize()  
+
+# # if sexo == 'F':  
+# #     print('Feminino')
+# # elif sexo == 'M':
+# #     print('Masculino')    
+# # else:
+# #     print('Valor Invalido')     
+
+# # 4. Faça um progrma que peça um valor e mostre na telal se o valor é positivo ou negativo
+
+# # numero = int(input('Digite um número:'))
+
+# # if numero >= 0 :  
+# #      print('Positivo')
+# # else:
+# #      print('Negativo')  
+
+# # 5. Dada a lista ['Corinthians',[1,2,3,4,5], 'Palmeiras', 'São Paulo',[10,11,12,13,14],'Flamengo', 'Vasco' ]
+# # Print o item 3, 13 e o vasco
+# # Print 5, SP e o 14 
+# # Mudar o valor do 4 para o 30 
+# # Mudar o valor do 10 para o 100
+# # Mudar o valor do 14 para 150 
+# # Mudar o valor do vasco para Bragantino 
+
+# lista = ['Corinthians',[1,2,3,4,5], 'Palmeiras', 'São Paulo',[10,11,12,13,14],'Flamengo', 'Vasco' ]
+
+# print(lista[1][2], lista[4][3], lista[6]) # Print o item 3, 13 e o vasco
+
+# print(lista[1][4], lista[3], lista[4][4]) # Print 5, SP e o 14 
+
+# lista[1][3] = 30 # Mudar o valor do 4 para o 30 
+
+# lista[4][0] = 100 # Mudar o valor do 10 para o 100
+
+# lista[4][-1] = 150 # Mudar o valor do 14 para 150 
+
+# lista[-1] = 'Bragantino' # Mudar o valor do vasco para Bragantino 
+
+# print (lista)
+
+
+# criar uma função que muda o valor de nome e printa na tela 
+
+# def mudanome(novo_nome):
+#     nome = novo_nome
+#     return nome
+
+# print (mudanome('Ana'))
+
+
+#criar um função que pega conteudo da variavel texto e deixa em caixa alta 
+
+# texto = 'Olá sou apenas um teste para esse exercicio.'
+
+# def uppertexto(text):
+
+#     return text.upper()
+
+# print(uppertexto(texto))
+
+
+
